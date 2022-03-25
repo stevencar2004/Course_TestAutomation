779a07b1209a7a621176e67efa74809e712d14a7
steven cardona <you@example.com>
2022-03-25 10:30:04 -0500

    Primera parte del proyecto - estructura y lectura de requerimientos

---
+++ src/main/java/com/basic_exercise/practicoprogramacionbasica/PracticoProgramacionBasica.java
@@ -0,0 +1,12 @@
+package com.basic_exercise.practicoprogramacionbasica;
+
+/**
+ *
+ * @author admin
+ */
+public class PracticoProgramacionBasica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
