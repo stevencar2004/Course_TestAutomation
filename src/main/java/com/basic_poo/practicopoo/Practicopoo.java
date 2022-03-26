028bff70bc924b4b9f16337ef70dbda9569687e9
steven cardona <you@example.com>
2022-03-26 13:47:48 -0500

    se inicia la practica sobre POO

---
+++ src/main/java/com/basic_poo/practicopoo/Practicopoo.java
@@ -0,0 +1,12 @@
+package com.basic_poo.practicopoo;
+
+/**
+ * FrontEnd Developer - Test Automation engineer, Jr
+ * @author Steven Cardona Betancur
+ */
+public class Practicopoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/basic_poo/practicopoo/Uruguayo.java
@@ -0,0 +1,9 @@
+package com.basic_poo.practicopoo;
+
+/**
+ * FrontEnd Developer - Test Automation engineer, Jr
+ * @author Steven Cardona Betancur
+ */
+public class Uruguayo {
+    
+}
